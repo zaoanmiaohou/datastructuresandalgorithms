08b4458cbac967f60726157274050eae9e77c9d7
zaoanmiaohou <13260010680@163.com>
2019-11-17 16:47:34 +0800

    数据结构与算法初次创建项目

---
+++ src/main/java/com/cathy/datastructuresandalgorithms/DatastructuresandalgorithmsApplication.java
@@ -0,0 +1,13 @@
+package com.cathy.datastructuresandalgorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatastructuresandalgorithmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatastructuresandalgorithmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cathy/datastructuresandalgorithms/DatastructuresandalgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cathy.datastructuresandalgorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatastructuresandalgorithmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
